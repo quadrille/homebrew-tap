03776cc9a4501ce04bd945fa51b1000d3af1c8a2
Moyukh Bera <moyukh@Moyukhs-MacBook-Pro.local>
2020-03-25 23:57:42 +0530

    added quadcli

--- Formula/quadrille.rb
+++ Formula/quadrille.rb
@@ -2,10 +2,11 @@ class Quadrille < Formula
   desc "The distrbuted, fault-tolerant Quadtree database"
   homepage "https://quadrille.io/"
   url "https://quadrilledb.s3.ap-south-1.amazonaws.com/quadrille-1.0.0.tar.gz"
-  sha256 "8ad2a46d10997fee530553bbadfba06f33dd910be848a3ab22a962c8c2d86749"
+  sha256 "9fc08ea3482598d837dd90cff4bf9b2c95d9afe5a38a61dc36d5e590db6b52a6"
   bottle :unneeded
 
   def install
     bin.install "quadrille"
+    bin.install "quadcli"
   end
 end
