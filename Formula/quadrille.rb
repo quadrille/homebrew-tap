33e746396ef71804b84bc5806f325e3be59f0506
Moyukh Bera <moyukh.bera@gmail.com>
2020-04-11 17:25:00 +0530

    updated homebrew artifact

--- Formula/quadrille.rb
+++ Formula/quadrille.rb
@@ -1,8 +1,8 @@
 class Quadrille < Formula
-  desc "The distrbuted, fault-tolerant Quadtree database"
+  desc "The lightweight, fault-tolerant, high performance geospatial database which supports blazing fast geoqueries"
   homepage "https://quadrille.io/"
-  url "https://quadrilledb.s3.ap-south-1.amazonaws.com/quadrille-1.0.0.tar.gz"
-  sha256 "9fc08ea3482598d837dd90cff4bf9b2c95d9afe5a38a61dc36d5e590db6b52a6"
+  url "https://artifacts.quadrille.io/quadrille-macOS-1.0.0.tar.gz"
+  sha256 "6c70646f3db6d0d1ea1f11328925204e6bac62aee66c1ba13d4d0f3f653e92b2"
   bottle :unneeded
 
   def install
